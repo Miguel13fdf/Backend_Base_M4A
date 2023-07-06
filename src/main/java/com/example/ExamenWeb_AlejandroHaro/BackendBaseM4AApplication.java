38bed98c2d1cbb130a7998b114d6c6534ff6764f
Miguel13fdf <122501535+Miguel13fdf@users.noreply.github.com>
2023-07-06 17:54:49 -0500

    Initial commit

---
+++ src/main/java/com/example/ExamenWeb_AlejandroHaro/BackendBaseM4AApplication.java
@@ -0,0 +1,14 @@
+package com.example.ExamenWeb_AlejandroHaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaseM4AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaseM4AApplication.class, args);
+		System.out.println("Exitoso");
+	}
+
+}
---
+++ src/test/java/com/example/ExamenWeb_AlejandroHaro/BackendBaseM4AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenWeb_AlejandroHaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBaseM4AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
